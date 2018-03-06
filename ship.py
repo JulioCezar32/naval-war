d85b0a0c2d6a6a37cc277087617c3b016d2f2167
Julio Cezar <jc.2013oliveira@gmai.com>
2018-03-06 00:04:09 -0300

    teste

---
+++ ship.py
@@ -0,0 +1,23 @@
+class Ship():
+
+    def __init__(self, tipo):
+        pass
+
+
+class PortaAvioes(Ship):
+
+    def __init__(self):
+        Ship.__init__(self,'Porta - Aviões')
+        return self
+
+    def posicao(self):
+        pass
+
+class Encouracado(Ship):
+
+    def __init__(self):
+        Ship.__init__(self,'Porta - Aviões')
+        return self
+
+    def posicao(self):
+        pass
